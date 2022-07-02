584b9f5b4ec67998af0cf3adb90b95648467a42b
vlkan <thkvolkanonder@gmail.com>
2022-07-02 21:33:16 +0300

    service

---
+++ src/main/java/com/springexample/account/service/AccountService.java
@@ -0,0 +1,13 @@
+package com.springexample.account.service;
+
+import com.springexample.account.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+    private final AccountRepository accountRepository;
+
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+}
